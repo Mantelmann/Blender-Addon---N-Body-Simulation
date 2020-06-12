28605f924a74e2889431edd8f5136f775e1ca145
Mantelmann <Mantelmann@users.noreply.github.com>
2020-06-12 21:53:23 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
 import N_Body_Blender
+import bpy
+
 print("Test")
 bl_info = {
     "name": "N-Body Simulation",
@@ -11,4 +13,23 @@ bl_info = {
     "internet": "https://github.com/Mantelmann/Blender-Addon---N-Body-Simulation"
 }
 
-N_Body_Blender.register()
+classes = (
+    N_Body_Blender.nbProperties,
+    N_Body_Blender.WM_OT_prepare_wrappers,
+    N_Body_Blender.WM_OT_calculate_keyframes,
+    N_Body_Blender.WM_OT_remove_wrappers,
+    N_Body_Blender.WM_OT_remove_keyframes,
+    N_Body_Blender.OBJECT_PT_CustomPanel
+)
+
+def register():
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+    bpy.types.Scene.n_body_sim = PointerProperty(type=N_Body_Blender.nbProperties)
+
+def unregister():
+    from bpy.utils import unregister_class
+    for cls in reversed(classes):
+        unregister_class(cls)
+    del bpy.types.Scene.n_body_sim
