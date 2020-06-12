bcb3872010abaff49a96189aa6b5a4cb5a4a1e10
Mantelmann <Mantelmann@users.noreply.github.com>
2020-06-12 18:35:20 +0200

    2.83 -> 2.80

--- N_Body_Addon_01.py
+++ N_Body_Addon_01.py
@@ -3,7 +3,7 @@ bl_info = {
     "description": "A Simple N-Body-Simulation Tool",
     "author": "Mantelmann",
     "version": (0, 0, 1),
-    "blender": (2, 83, 0),
+    "blender": (2, 80, 0),
     "location": "3D View > Create",
     "category": "Animation",
     "internet": "https://github.com/Mantelmann/Blender-Addon---N-Body-Simulation"
