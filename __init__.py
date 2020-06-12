a5905e4638e9ac9860c21ec60ff5923583e98b4a
Mantelmann <Mantelmann@users.noreply.github.com>
2020-06-12 21:42:32 +0200

    add bl_info

--- __init__.py
+++ __init__.py
@@ -1,3 +1,14 @@
 from N_Body_Blender import register
 print("Test")
+bl_info = {
+    "name": "N-Body Simulation",
+    "description": "A Simple N-Body-Simulation Tool",
+    "author": "Mantelmann",
+    "version": (0, 0, 1),
+    "blender": (2, 80, 0),
+    "location": "3D View > Create",
+    "category": "Animation",
+    "internet": "https://github.com/Mantelmann/Blender-Addon---N-Body-Simulation"
+}
+
 register()
